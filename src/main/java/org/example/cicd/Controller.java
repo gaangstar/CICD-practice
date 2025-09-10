7c0b65309875c91b2c510914815f9eaa96708877
yhg <hghgy@hansung.ac.kr>
2025-09-10 10:12:33 +0900

    first commit

---
+++ src/main/java/org/example/cicd/Controller.java
@@ -0,0 +1,12 @@
+package org.example.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("test");
+    }
+}
